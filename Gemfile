6bb97ab7bca6700b1070b6658e85a404ff41dc8f
tom-cheung <wai.tomcheung@gmail.com>
2020-12-07 17:32:53 -0800

    created user model and controller

--- Gemfile
+++ Gemfile
@@ -43,6 +43,9 @@ group :development, :test do
   gem 'pry-rails'
   gem 'better_errors'
   gem 'binding_of_caller'
+  gem 'faker'
+  gem 'better_errors'
+  gem 'jquery-rails'
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+User.destroy_all 
+
+user1 = User.create(first_name: 'Michelle', last_name: 'Tester1', email: 'tester1@gmail.com', password: '123456')
+user2 = User.create(first_name: 'Steve', last_name: 'Tester2', email: 'tester2@gmail.com', password: '123456')
+user3 = User.create(first_name: 'Chris', last_name: 'Tester3', email: 'tester3@gmail.com', password: '123456')
+
