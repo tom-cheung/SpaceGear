01c1cfb4a0670a7a8d83b1875e797b0b379570f5
tom-cheung <wai.tomcheung@gmail.com>
2021-01-27 11:12:33 -0800

    refactor calcTotal() to work with useEffect hook

--- frontend/components/order/order_show.jsx
+++ frontend/components/order/order_show.jsx
@@ -4,68 +4,77 @@ import OrderedProduct from './ordered_products';
 
 const OrderShow = (props) => {
     
-    const [order, updateOrder] = useState(props.order)
+    const [order, updateOrder] = useState(Object.assign({}, props.order))
     const [orderedProducts, updateProducts] = useState([...props.order.ordered_product])
+    const [total, updateTotal] = useState(props.order.total)
 
     useEffect(() => {
         let inputs = document.getElementsByTagName("INPUT")
         for(let i = 0; i < inputs.length; i++) {
             inputs[i].value = orderedProducts[i].quantity
         }
+        
     })
+
+    useEffect(() => {
+        calcTotal()
+    }, [orderedProducts])
 
 
     const updateQuantity = (e, id) => {
-        console.log(e)
-        console.log(id)
-        updateProducts(orderedProducts.map(product => {
+        updateProducts(
+            orderedProducts.map(product => {
             if(product.id === id) {
                 product.quantity = parseInt(e.currentTarget.value);
                 return product;
             } else {
                 return product; 
             }
-        }))
+            })
+        )
     }
 
-    console.log(orderedProducts)
+    const removeItem = (id) => {
+        updateProducts( orderedProducts.filter((product) => product.id !== id ) )
+    }
+
+    const calcTotal = () => {
+
+        if(Object.values(props.products).length) {
+
+            let newTotal = 0; 
+            orderedProducts.forEach(order => {
+                newTotal += (order.quantity * parseInt(props.products[order.product_id].price))
+            })
+            updateTotal(newTotal)
+        }
+    }
 
     return (
-
         <div>
             
-
+            {console.log(orderedProducts)}
          
             {Object.values(props.order).length ? 
 
                     <div className="order-show-container">
                         <h1>Order Number: {order.id}</h1>
-                        <h1>Order Total: ${parseFloat(order.total).toFixed(2)}</h1>
+                        <h1>Order Total: ${parseFloat(total).toFixed(2)}</h1>
                         <h1>Ordered Products:</h1>
 
                         {orderedProducts.map((details) => {
                             return <div>
                                         <OrderedProduct key={details.id} details={details} product={props.products[details.product_id]}/> 
-                                        <input type="number" onChange={(e) => updateQuantity(e, details.id)} placeholder={details.quantity}/>
-                                        <button onClick={() => {updateProducts(orderedProducts.filter(product => product.id != details.id) ) }}>Remove Item</button>
+                                        <input type="number" onChange={(e) => updateQuantity(e, details.id)} placeholder={details.quantity} min='0'/>
+                                        {/* <button onClick={() => {updateProducts(orderedProducts.filter(product => product.id != details.id) ) }}>Remove Item</button> */}
+                                        <button onClick={() => removeItem(details.id)}>Remove Item</button>
                                     </div>
                         })}
-                        {/* <div>
-                            <h1>Order Show and Edit Form</h1>
-                            <h1>Order # {order.id}</h1>
-                            <h1>Total ${parseFloat(order.total).toFixed(2)}</h1>
-                            {orderedProducts.map((order, idx) => {
-                                return (
-                                    <div key={idx}>
-                                        <p>{order.quantity}</p>
-                                        {order.product ? <li key={idx}>{order.product.product_name}</li> : null}
-                                    </div>
-                                )
-                            })}
-                        
-                            <button onClick={() => this.props.deleteOrder(order.id)}>Cancel Order</button>
-                        </div> */}
-                    </div>        
+
+                        <button>Update Order</button>
+
+                    </div>   
+                    
                                     
                 : 
                 
