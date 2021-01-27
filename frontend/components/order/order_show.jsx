fff5d79d1c348c565a82bdbb9de7399fcb061f5f
tom-cheung <wai.tomcheung@gmail.com>
2021-01-26 21:28:56 -0800

    implement useEffect to reset input values after deleting products

--- frontend/components/order/order_show.jsx
+++ frontend/components/order/order_show.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {Link, Redirect} from 'react-router-dom';
 import OrderedProduct from './ordered_products';
 
@@ -7,14 +7,33 @@ const OrderShow = (props) => {
     const [order, updateOrder] = useState(props.order)
     const [orderedProducts, updateProducts] = useState([...props.order.ordered_product])
 
-    updateQuantity = (e) => {
+    useEffect(() => {
+        let inputs = document.getElementsByTagName("INPUT")
+        for(let i = 0; i < inputs.length; i++) {
+            inputs[i].value = orderedProducts[i].quantity
+        }
+    })
 
+
+    const updateQuantity = (e, id) => {
+        console.log(e)
+        console.log(id)
+        updateProducts(orderedProducts.map(product => {
+            if(product.id === id) {
+                product.quantity = parseInt(e.currentTarget.value);
+                return product;
+            } else {
+                return product; 
+            }
+        }))
     }
+
+    console.log(orderedProducts)
 
     return (
 
-        
         <div>
+            
 
          
             {Object.values(props.order).length ? 
@@ -23,19 +42,12 @@ const OrderShow = (props) => {
                         <h1>Order Number: {order.id}</h1>
                         <h1>Order Total: ${parseFloat(order.total).toFixed(2)}</h1>
                         <h1>Ordered Products:</h1>
-                        {orderedProducts.map((details, idx) => {
+
+                        {orderedProducts.map((details) => {
                             return <div>
                                         <OrderedProduct key={details.id} details={details} product={props.products[details.product_id]}/> 
+                                        <input type="number" onChange={(e) => updateQuantity(e, details.id)} placeholder={details.quantity}/>
                                         <button onClick={() => {updateProducts(orderedProducts.filter(product => product.id != details.id) ) }}>Remove Item</button>
-
-                                        <input type="number" onChange={(e) => { updateProducts( orderedProducts.map(product => {
-                                            if(product.id === details.id) {
-                                                product.quantity = parseInt(e.currentTarget.value);
-                                                return product;
-                                            } else {
-                                                return product
-                                            }
-                                        }) ) }}/>
                                     </div>
                         })}
                         {/* <div>
