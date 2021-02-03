77bf776aa22b5599efbd21621ff4ba6014a1c239
tom-cheung <wai.tomcheung@gmail.com>
2021-02-03 12:37:09 -0800

    restyle header bar and add searchbar container to header

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -7,41 +7,56 @@ class Header extends React.Component {
     constructor(props) {
         super(props)
        
+        this.state = {
+            showSearch: false,
+        }
     }
 
     componentDidMount() {
         this.props.fetchProducts(); 
     }
 
+    // searchBar = (e) => {
+    //     console.log('hi');
+    // }
     
     render() {
         return (
         <div id="mainHeader">
-            
-            <div id="headerLeft">
-                <div id="mainLogo" className="animate__animated animate__fadeIn">
-                    <Link to="/"><img src={window.mainLogo} alt="" width="100" height="100"/></Link>
-                </div>
 
-                <div id="product-categories">
+            <div className="header-top">
+
+                <div id="headerLeft">
+                    <div id="mainLogo" className="animate__animated animate__fadeIn">
+                        <Link to="/"><img src={window.mainLogo} alt="" width="150" height="150"/></Link>
+                    </div>
+
+                    <div id="product-categories">
+                        <ul>
+                            {this.props.categories.map((category, idx) => {
+                                return <li key={category.id}>
+                                        <HeaderDropdown key={idx} categoryName={category.category_name} categoryId={category.id} categoryTypes={category.product_types}/>
+                                        </li>
+                            })} 
+                        </ul>
+                    </div>
+                </div>
+            
+
+                <div id="userTools">
                     <ul>
-                        {this.props.categories.map((category, idx) => {
-                            return <li key={category.id}>
-                                    <HeaderDropdown key={idx} categoryName={category.category_name} categoryId={category.id} categoryTypes={category.product_types}/>
-                                    </li>
-                        })} 
+                        <li><Link to="/account">Account</Link></li> 
+                        <li><span>Search</span></li>
+                        <li><Link to="/cart">Cart</Link></li>
+                        {/* want to add in a link to the account here, will likely need to import Link above, and use Link tags */}
                     </ul>
                 </div>
-            </div>
-            
 
-            <div id="userTools">
-                <ul>
-                    <li><Link to="/account">Account</Link></li> 
-                    <li><a href="">Search</a></li>
-                    <Link to="/cart">Cart</Link>
-                    {/* want to add in a link to the account here, will likely need to import Link above, and use Link tags */}
-                </ul>
+
+            </div>
+
+            <div id="search-bar">
+                <p>hello</p>
             </div>
 
         </div>
