3936764f073ddf596add481379df166dd408650e
tom-cheung <wai.tomcheung@gmail.com>
2021-02-04 14:26:35 -0800

    update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+OrderedProduct.destroy_all
+Order.destroy_all
+Contact.destroy_all
 Product.destroy_all
 ProductType.destroy_all
 Category.destroy_all 
