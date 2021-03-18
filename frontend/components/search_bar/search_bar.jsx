fe20a3d724d92b6b758058310b9e6dce8c4bc7d5
tom-cheung <wai.tomcheung@gmail.com>
2021-03-17 23:28:18 -0400

    removed comments

--- frontend/components/search_bar/search_bar.jsx
+++ frontend/components/search_bar/search_bar.jsx
@@ -3,19 +3,21 @@ import { Link } from 'react-router-dom'
 
 const SearchBar = ({products, categories}) => {
 
+    // searchResults is an array containing all products that match the user input 
     const [searchResults, updateResult] = useState([]);
-    const [prevSearches, updateSearch] = useState([])
-        // save each search as a key in an object, the value being an array of words
-        // keep this as an object 
-        // during each subsequent search, check is the term exists in the prevSearches, if so just return the results   
-        // save this to the DB? would this optimize search? 
+
+    // prevSearches is an object where keys are previous search terms and values are the products 
+    const [prevSearches, updateSearch] = useState([]);   
+        // saving this to the database may optimize search, would only have to pull it each time this component mounts  
     
+    // products seperated by category, which is used as a preview in the search results 
     const [mens, updateMens] = useState({});
     const [womens, updateWomens] = useState({});
     const [kids, updateKids] = useState({});
     const [misc, updateMisc] = useState({});
     const [searchTerm, UpdateTerm] = useState("");
 
+    // grabs the ID of each each product category 
     let mensID = categories.find(cat => cat.category_name === "Mens").id;
     let womensID = categories.find(cat => cat.category_name === "Womens").id;
     let childrensID = categories.find(cat => cat.category_name === "Children").id;
@@ -23,35 +25,37 @@ const SearchBar = ({products, categories}) => {
 
 
     const search = (e) => {
+        // clears the search bar of previous inputs 
         updateMens({});
         updateWomens({});
         updateKids({});
         updateMisc({});
 
+        // if the search input is a key within prevSearches then we'll just return the previous results 
         if(prevSearches[e.target.value] !== undefined) {
 
+            //updates the search result local state 
             updateResult(prevSearches[e.target.value]);
+
+            // saves the search term to be used when user clicks to see all search results. Used to run the search again in the search index component
             UpdateTerm(e.target.value);
 
+            // seperates the products by their category for search result preview 
             prevSearches[e.target.value].forEach(product => {
                     if(product.category_id === mensID ) {
                         updateMens(product)
-                        // && Object.values(mens).length <= 1 
                     }
 
                     if(product.category_id === womensID) {
                         updateWomens(product)
-                        // && Object.values(womens).length <= 1
                     }
 
                     if(product.category_id === childrensID) {
                         updateKids(product)
-                        // && Object.values(kids).length <= 1
                     }
 
                     if(product.category_id === miscID) {
                         updateMisc(product)
-                        // && Object.values(misc).length <= 1
                     }
             })
         
@@ -60,38 +64,43 @@ const SearchBar = ({products, categories}) => {
 
             let results = [];
 
+            // does not pull results based on empty string or an empty space 
             if(e.target.value !== "" && e.target.value !== " ") {
 
-                products.map(product => {
-                    // console.log(product.product_name.includes(e.currentTarget.value))
+                // maps through all the products and pushes all products whose name matches the search input into the results array
+                products.map(product => { 
+
                     if(product.product_name.toLowerCase().includes((e.target.value).toLowerCase())) {
+
                         results.push(product)
+
                         if(product.category_id === mensID) {
                             updateMens(product)
-                            // && Object.values(mens).length <= 1 
                         }
 
                         if(product.category_id === womensID) {
                             updateWomens(product)
-                            // && Object.values(womens).length <= 1
                         }
 
                         if(product.category_id === childrensID) {
                             updateKids(product)
-                            // && Object.values(kids).length <= 1
                         }
 
                         if(product.category_id === miscID) {
                             updateMisc(product)
-                            // && Object.values(misc).length <= 1
                         }
                     }
                 })
 
             }
 
+            //updates the search result local state  
             updateResult(results);
+            
+            // saves the search term to be used when user clicks to see all search results. Used to run the search again in the search index component
             UpdateTerm(e.target.value);
+
+            // updates the prevSearches object so if a user retypes the same input we do not have to iterate through all products again
             updateSearch(Object.assign({}, prevSearches, {[e.target.value]: results}))
 
         }
